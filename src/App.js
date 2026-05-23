f74d09925d1522567271b60520953a0283f8e229
abuthahir s <abuthahir.s@unisys.com>
2026-05-23 17:56:54 +0530

    latest image

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ function App() {
 
   return (
     <div className="App">    
+    <h2>Image latest</h2>
       <AddProducts
         categoryOptions={opt}
         onAddProduct={handleAddProducts}
